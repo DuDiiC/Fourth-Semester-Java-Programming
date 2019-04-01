5e335c1edb469315d1d6492121261cdeef8c5a72
Maciej Dudek <maciejdudek96@wp.pl>
2019-04-01 20:01:01 +0200

    Add 2nd task from 5th lab
    - if I want unusing null in tab, I need Double object, not double basic type
    - if I want catch NullPointerException in parent class, I have to do it with the Exception too, because it's more general exception

--- Lab05/Task1-2/src/com/company/CatchThrows.java
+++ Lab05/Task1-2/src/com/company/CatchThrows.java
@@ -2,18 +2,14 @@ package com.company;
 
 public class CatchThrows {
 
-    public void calculateReverse(double[] tab) {
+    public void calculateReverse(Double[] tab) throws NullPointerException, Exception {
 
-        for(int i = 0; i < tab.length; ++i) {
+        for(int i = 0; i <= tab.length; ++i) {
             try {
                 tab[i] = 1.0 / tab[i];
                 System.out.println(tab[i]);
-            } catch (NullPointerException e) {
-                System.err.println("Throw NullPointerException in " + i + " position.");
             } catch (ArithmeticException e) {
                 System.err.println("Throw ArithmeticException in " + i + " position.");
-            } catch (Exception e) {
-                System.err.println("Another exception in " + i + " position.");
             }
         }
     }
--- Lab05/Task1-2/src/com/company/Main.java
+++ Lab05/Task1-2/src/com/company/Main.java
@@ -3,9 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double[] tab = new double[] {3.4, 23, 2.3, 1.0/3};
+        Double[] tab = new Double[] {3.4, 23.0, 2.3, 1.0/3};
+        //tab[2] = null;
         CatchThrows catchThrows = new CatchThrows();
 
-        catchThrows.calculateReverse(tab);
+        try {
+            catchThrows.calculateReverse(tab);
+        } catch(NullPointerException e) {
+            System.err.println("Throw NullPointerException in tab.");
+        } catch (Exception e) {
+            System.err.println("Another Exception in tab.");
+        }
     }
 }
