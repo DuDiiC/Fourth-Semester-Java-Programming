aeedce021571d556a169dc80c5a594d3153e6c25
Maciej Dudek <maciejdudek96@wp.pl>
2019-04-02 21:47:17 +0200

    Modify 1st task from 6th lab
    Easier way to get the path.

--- Lab06/Task1/src/com/company/Main.java
+++ Lab06/Task1/src/com/company/Main.java
@@ -1,18 +1,9 @@
 package com.company;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) {
-        ContentsOfCatalog contentsOfCatalog = null;
-        try {
-            // using the workspace catalog
-            contentsOfCatalog = new ContentsOfCatalog(new File(".").getCanonicalPath()+"\\");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ContentsOfCatalog contentsOfCatalog = new ContentsOfCatalog(System.getProperty("user.dir"));
         contentsOfCatalog.writeTheContent();
     }
 }
