7f577650ac0d88b928ea50bfbd5097187b6e4898
Maciej Dudek <maciejdudek96@wp.pl>
2019-04-17 21:20:16 +0200

    Add task 1st from 8th lab v3
    v1 - my own thread class that inherits from the Thread class
    v2 - my own thread class that implements the Runnable interface
    v3 - anonymous class in the Main method

--- Lab08/Task1-2/src/com/company/Main.java
+++ Lab08/Task1-2/src/com/company/Main.java
@@ -1,10 +1,29 @@
 package com.company;
 
+import java.util.Random;
+
+import static java.lang.Thread.sleep;
+
 public class Main {
 
     public static void main(String[] args) {
-    	
-	    Thread t1 = new Thread(new ThreadInterface());
+
+	    Thread t1 = new Thread() {
+			@Override
+			public void run() {
+				Random random = new Random();
+				for(int i = 0; i < 100; i++) {
+					int sleepTime = Math.abs(random.nextInt()%1000);
+					Thread thread = Thread.currentThread();
+					System.out.println("Thread " + thread.getId() + " number " + i);
+					try {
+						sleep(sleepTime);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
 	    Thread t2 = new Thread(new ThreadInterface());
 	    Thread t3 = new Thread(new ThreadInterface());
 	    Thread t4 = new Thread(new ThreadInterface());
